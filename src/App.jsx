766fc2ecaed1f6e10e16f886c963f1f847b42cb2
Rukaiya Bano <rukaiyabano888@gmail.com>
2024-02-06 19:44:26 -0800

    upload files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import MainPage from './MainPage.jsx';
+import Nav from './nav.jsx';
+import './App.css';
+
+function App() {
+
+  return (
+    <> 
+    <Nav/>
+    <MainPage/>
+    </>
+)
+}
+
+export default App;
---
+++ src/nav.jsx
@@ -0,0 +1,23 @@
+
+
+const Nav = () => {
+    return (
+    <>
+    <nav>
+        <div className="logo">
+            <img src="./public/nike.png" alt="" />
+        </div>
+        <ul>
+            <li href="#">Menu</li>
+            <li href="#">Help</li>
+            <li href="#">About</li>
+            <li href="#">Contact</li>
+        </ul>
+        <button className='btn'>login</button>
+    </nav>
+    </>
+)
+}
+
+
+export default Nav;
